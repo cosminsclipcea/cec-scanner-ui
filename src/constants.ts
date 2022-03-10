cfeab3d986174c7bb18c3382c90cc95a58491d3f
cosminsclipcea <51709979+cosminsclipcea@users.noreply.github.com>
2022-03-10 11:51:20 +0200

    initial commit

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
-export const API_URL = "https://192.168.1.172:8744";
+export const API_URL = "https://scanner-api"; //"https://scanner-api"; //"https://192.168.0.103:8744";
 export const API_USERNAME = "user";
 export const API_PASSWORD = "passwd";
+export const CONNECTION_TIMEOUT = 25;
